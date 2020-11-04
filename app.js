f6013fd5227194f891795911029d4590739bbb1f
Carolina Urrea <carolinaurrea@Carolinas-MacBook-Pro.local>
2020-11-04 17:34:09 -0500

    [app.js index.html style.css]added canvas image and backgroud to game

--- app.js
+++ app.js
@@ -1,203 +1,89 @@
 const movementDisplay = document.querySelector('#movement')
 const game = document.querySelector('#game')
-
+const image = document.getElementById('krabby')
+// const image = document.getElementById('broccoli')
 // syncing up the canvas's internal height&width to its apparent height&width
 const computedStyle = getComputedStyle(game)
 const height = computedStyle.height
 const width = computedStyle.width
-game.height = height.replace('px', '')
-game.width = width.replace('px', '')
+// game.height = height.replace('px', '')
+// game.width = width.replace('px', '')
 
 
 
 // grab a context from the canvas
-let player = new Burger(325, 250)
+const ctx = game.getContext('2d')
 
-
-
-class Crawler {
-  constructor(x, y, color, width, height) {
-    this.x = x
-    this.y = y
-    this.color = color
-    this.width = width
-    this.height = height
-    this.alive = true
-  }
-
-  render() {
-    ctx.fillStyle = this.color
-    ctx.fillRect(this.x, this.y, this.width, this.height)
-  }
+class Sprite {
+    constructor(x, y, width, height) {
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.alive = true
+    }
+    render() {
+        ctx.drawImage(image, this.x, this.y, this.width, this.height)
+    }
 }
-const [Burger] = document.createElement('img')
-player.setAttribute('src', './img/Krabby_Patty.png')
-// const ogre = new Crawler(10, 10, "#BADA55", 40, 80)
-// const hero = new Crawler(150, 150, 'hotpink', 20, 20)
+class Projectile {
+    constructor(x, y, width, height){
+        this.x = x
+        this.y = y
+        this.width = width
+        this.height = height
+        this.velocity = velocity
+    }
+}
+
+const player = new Sprite(145, 140, 10, 10)
+
+
+player.render()
 
 // document.getElementById('status').addEventListener('click', function() {
-//   hero.render()
-//   ogre.render()
+//   veggies.render()
 // })
 
-// document.addEventListener('keyup', function(evt) {
-//   if (evt.key === 'w') {
-//     hero.y -= 10
-//   } else if (evt.key === 'a') {
-//     hero.x -= 10
-//   } else if (evt.key === 's') {
-//     hero.y += 10
-//   } else if (evt.key === 'd') {
-//     hero.x += 10
-//   }
-
-//   movementDisplay.textContent = `X: ${hero.x}, Y: ${hero.y}`
-// })
+document.addEventListener('keydown', function(evt) {
+  if (evt.key === 'ArrowUp' && player.y > 110) {
+    player.y -= 10
+  } else if (evt.key === 'ArrowLeft' && player.x > 0) {
+    player.x -= 10
+  } else if (evt.key === 'ArrowDown' && player.y < 140) {
+    player.y += 10
+  } else if (evt.key === 'ArrowRight' && player.x < 290) {
+    player.x += 10
+  }
+  console.log(player)
+  movementDisplay.textContent = `X: ${player.x}, Y: ${player.y}`
+})
 
 // function detectHit() {
 //   // hit coming in from the right
-//   if (hero.x < ogre.x + ogre.width
-//     && hero.x + hero.width > ogre.x
-//     && hero.y < ogre.y + ogre.height
-//     && hero.y + hero.height > ogre.y) {
-//     ogre.alive = false
+//   if (player.x < veggies.x + veggies.width
+//     && player.x + player.width > veggies.x
+//     && player.y < veggies.y + veggies.height
+//     && player.y + player.height > veggies.y) {
+//     veggies.alive = false
 //   }
 // }
 
-// function rePaint() {
-//   // clear off the entire canvas
-//   ctx.clearRect(0, 0, game.width, game.height)
-
-//   // render the hero and the ogre
-//   hero.render()
-//   if (ogre.alive) {
-//     ogre.render()
+function rePaint()  {
+  // clear off the entire canvas
+  ctx.clearRect(0, 0, game.width, game.height)
+    player.render()
+}
+  //render the player and the veggies
+//   player.render()
+//   if (vegies.alive) {
+//     veggies.render()
 //   }
 
 //   detectHit()
-// }
-
-// setInterval(rePaint, 1000 / 60) // 60 frames per second
-
-
-
-// // const movementDisplay = document.querySelector('#movement')
-// // const game = document.querySelector('#game')
-
-// // const computedStyle = getComputedStyle(game)
-// // const height = computedStyle.height;
-// // const width = computedStyle.width;
-
-// // game.height = parseInt(height)
-// // game.width = parseInt(width)
-
-// // const ctx = game.getContext('2d')
-// // // ctx.fillStyle = 'white';
-// // // ctx.strokeStyle = 'red'
-// // // ctx.lineWidth = 5;
-
-// // // // draw unsing the context
-// // // ctx.fillRect(10, 10, 100, 100);
-// // // ctx.strokeRect(10, 10, 100, 100);
-
-// // // function drawBox(x, y, size, color){
-// // //     ctx.fillStyle = color;
-// // //     ctx.fillRect(x, y , size, size)
-// // // }
-// // // document.getElementById('status').addEventListener('click,')
-
-// // // drawBox(200, 200, 50, 'pink');
-// // // drawBox(300, 300, 50, 'pink');
+//}
+ setInterval(rePaint, 1000 / 60) // 60 frames per second
 
 
 
-// // class Crawler {
-// //     constructor(x, y, color, width, height) {
-// //         this.x = x
-// //         this.y = y
-// //         this.color = color
-// //         this.width = width
-// //         this.height = height
-// //         this.alive = true
-// //     }
-// //     render() {
-// //         ctx.fillStyle = this.color
-// //         ctx.fillRect(this.x, this.y, this.width, this.height)
-// //     } 
-// // }
-// // const ogre = new Crawler(10, 10, '#BADA55', 40, 80)
-// // const hero = new Crawler(0, 0, 'hotpink', 20, 20)
 
-// // console.log(ogre);
-// // console.log(hero);
-
-
-
-// // document.getElementById('status').addEventListener('click', function() {
-// //     hero.render()
-// //     ogre.render()
-// // })
-
-// // document.addEventListener('keyup', function(evt) {
-// //     if (evt.key === 'w'){
-// //         hero.y -= 10
-// //     } else if (evt.key === 'a'){
-// //         hero.x -= 10
-// //     } else if (evt.key === 's'){
-// //         hero.y += 10
-// //     } else if (evt.key === 'd'){
-// //         hero.x += 10
-// //     }
-    
-// //     movementDisplay.textContent = `X: ${hero.x}, Y: ${hero.y}`
-// // })
-
-// // function detectHit() {
-// //     // hit coming in from the right
-// //     if (hero.x < ogre.x + ogre.width
-// //       && hero.x + hero.width > ogre.x
-// //       && hero.y < ogre.y + ogre.height
-// //       && hero.y + hero.height > ogre.y) {
-// //       ogre.alive = false
-// //       }
-// // }
-
-// // function rePaint(){
-// //     ctx.clearRect(0, 0, game.width, game.height)
-   
-   
-// //     hero.render()
-// //     if (ogre.alive) {
-// //       ogre.render()
-// //     }
-// //     detectHit()
-// // }
-
-
-// // setInterval(rePaint, 1000 / 60)
-
-// // // var ogre = {
-// // //     x: 10,
-// // //     y: 10,
-// // //     color: "#BADA55",
-// // //     width: 40,
-// // //     height: 80,
-// // //     alive: true,
-// // //     render: function () {
-// // //       ctx.fillStyle = this.color
-// // //       ctx.fillRect(this.x, this.y, this.width, this.height)
-// // //     }
-// // //   }
-    
-// // //   var hero = {
-// // //     x: 0,
-// // //     y: 0,
-// // //     color: "hotpink",
-// // //     width: 20,
-// // //     height: 20,
-// // //     alive: true,
-// // //     render: function () {
-// // //       ctx.fillStyle = this.color
-// // //       ctx.fillRect(this.x, this.y, this.width, this.height)
-// // //     }
-// // //   }
